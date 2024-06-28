04f375912d1006d9f926bb6afb84830467380cad
1171384960@qq.com <1171384960@qq.com>
2024-06-28 09:14:11 +0800

    1

--- src/robot_control/robot_camera_control/scripts/color_detect.py
+++ src/robot_control/robot_camera_control/scripts/color_detect.py
@@ -9,6 +9,8 @@ from base_detect import BaseDetection
 import os
 import yaml
 
+current_work_dir = os.path.dirname(__file__)
+
 
 class ColorDetectServer(BaseDetection):
     def __init__(self, config):
@@ -24,21 +26,27 @@ class ColorDetectServer(BaseDetection):
 
     def preprocess_image(self, cv_image):
         hsv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
-
+        x, y = 0, 0
         # 检查是否存在roi_min和roi_max的配置
         if "roi_min" not in self.color_threshold or "roi_max" not in self.color_threshold:
             rospy.logwarn("no ROI.")
-            return cv_image
-        
+            return cv_image, (x, y)
+
         # 定义ROI区域的HSV范围
         lower_hsv = np.array(self.color_threshold["roi_min"])
         upper_hsv = np.array(self.color_threshold["roi_max"])
-
         mask = cv2.inRange(hsv_image, lower_hsv, upper_hsv)
+
+        # 进行形态学开运算去除噪点
+        kernel1 = np.ones((9, 9), np.uint8)
+        morph_open = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel1)
+        # 添加闭运算操作以合并连通域
+        kernel2 = np.ones((5, 5), np.uint8)
+        morph_close = cv2.morphologyEx(morph_open, cv2.MORPH_CLOSE, kernel2)
 
         # 查找roi轮廓
         contours, _ = cv2.findContours(
-            mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            morph_close, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
         # 绘制roi边界框
         if contours:
@@ -48,20 +56,26 @@ class ColorDetectServer(BaseDetection):
             cv2.rectangle(cv_image, (x, y), (x+w, y+h), (0, 255, 0), 2)
             roi = cv_image[y:y+h, x:x+w]  # 提取roi区域
 
-        return roi
+        return roi, (x, y)
 
-    def color_thresholding(self, cv_image):
+    def color_thresholding(self, cv_image,f, x=0, y=0):
         hsv_image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
-
         # 读取配置文件中的颜色阈值
         lower_color = np.array(self.color_threshold["lower"])
         upper_color = np.array(self.color_threshold["upper"])
 
         # 应用颜色阈值
         mask = cv2.inRange(hsv_image, lower_color, upper_color)
+        image_path = os.path.join(current_work_dir, "2.png")
+        cv2.imwrite(image_path, mask)
 
         contours, _ = cv2.findContours(
             mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        if f==False:
+        # 滤除h>w的干扰轮廓
+            contours = [cnt for cnt in contours if cv2.boundingRect(
+                cnt)[2] >= cv2.boundingRect(cnt)[3]]  
 
         # 按图像y坐标对轮廓进行排序(从上到下)
         contours = sorted(
@@ -79,23 +93,22 @@ class ColorDetectServer(BaseDetection):
                 # 计算物体中心点和角度
                 center_x, center_y = rect[0]
                 angle = rect[2]
-
                 cv2.drawContours(cv_image, [box], 0, (0, 255, 0), 2)
                 cv2.circle(cv_image, (int(center_x), int(center_y)),
                            5, (0, 0, 255), -1)
 
                 color = "detected_color"
 
+                center_x += x
+                center_y += y
                 objects_info.append({
                     'label': color,
                     'center_x': int(center_x),
                     'center_y': int(center_y),
                     'angle': angle
                 })
-        
-        # cv2.imshow("Color Detection", cv_image)
-        # cv2.waitKey(0) 
-
+        image_path = os.path.join(current_work_dir, "1.png")
+        cv2.imwrite(image_path, cv_image)
         return objects_info
 
     def handle_color_detection(self, request):
@@ -105,16 +118,20 @@ class ColorDetectServer(BaseDetection):
         if color_name not in self.colors:
             rospy.logerr(f"Color '{color_name}' not found in configuration")
             return Hand_CatchResponse(labels=[], positions=[])
-        
+
         self.threshold = self.colors[color_name]
         self.color_threshold = self.threshold["color_threshold"]
         self.area_threshold = self.threshold["area_threshold"]
-
+        f=False
         if request.run:
             if self.cv_image is not None:
-                self.cv_image = self.preprocess_image(self.cv_image)
+                self.cv_image, (x, y) = self.preprocess_image(self.cv_image)
+                if color_name != "cabinet_handle" :
+                    f=True
+                else:
+                    f=False
                 objects_info = self.color_thresholding(
-                    self.cv_image)
+                    self.cv_image,f, x, y)
 
                 for obj in objects_info:
                     label = obj['label']
@@ -136,7 +153,6 @@ class ColorDetectServer(BaseDetection):
 
 
 if __name__ == '__main__':
-    current_work_dir = os.path.dirname(__file__)
     config_path = os.path.join(current_work_dir, "config", "color.yaml")
     with open(config_path, "r") as config_file:
         config = yaml.safe_load(config_file)
